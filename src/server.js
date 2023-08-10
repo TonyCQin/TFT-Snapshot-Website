9f2bdc9e62d70f81524cc7d9367d677d770832f3
Tony Qin <qin.tony123@gmail.com>
2023-08-10 13:57:38 -0400

    idk

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.post("/api/updateStats", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port 8080");
+  console.log("Server is running on port " + PORT);
 });
 
 let indexFile;
